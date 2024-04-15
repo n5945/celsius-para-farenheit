28646289f9c19e9b1cfb3842e582b632291ff220
n5945 <162457150+n5945@users.noreply.github.com>
2024-04-15 09:08:02 -0300

    Add files via upload

---
+++ cParaF.js
@@ -0,0 +1,24 @@
+//C = (F - 32)
+//F = C * 9/5 + 32
+
+/*function conversaoCelsius(farenheit) {
+    const conversao = (farenheit - 32) * 5/9
+    
+    return conversao;
+}
+function
+conversaoFarenheit(celsius){
+    const conversao = celsius = 9/5 + 32;
+
+return conversao
+}
+
+console.log(conversaoCelsius(90).toFixed(2));
+console.log(conversaoFarenheit(32.22).toFixed(2));
+*/
+
+function farenheitToCelsius(temperatura){
+const celsius = temperatura * 9/5 + 32
+return (farenheit)
+
+}
